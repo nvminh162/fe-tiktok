2c75e8891f602e1f51e2c3bc617fa7c9ac9da6dc
nvminh162 <nvminh162@gmail.com>
2025-02-04 23:47:40 +0700

    change routes index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,1 @@
-import { HeaderOnly } from '~/layouts';
-import { Home, Following, Profile, Upload, Search } from '~/pages';
-import config from '~/config';
-
-const publicRoutes = [
-    { path: config.routes.home, element: Home },
-    { path: config.routes.following, element: Following },
-    { path: config.routes.profile, element: Profile },
-    { path: config.routes.upload, element: Upload, layout: null },
-    { path: config.routes.search, element: Search, layout: HeaderOnly },
-];
-
-const privateRoutes = [];
-
-export { publicRoutes, privateRoutes };
+export { publicRoutes, privateRoutes } from './routes'
